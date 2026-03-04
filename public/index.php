1a426fa9d3f35a53cf4a3194a109dadc23684313
Said Abdulsalam <sabdulsalam@ubtconsults.se>
2026-03-04 11:27:57 +0100

    fix

--- public/index.php
+++ public/index.php
@@ -4,29 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
-// Serve ACME HTTP-01 challenge files early, regardless of Laravel route cache.
-$requestPath = (string) (parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?? '/');
-$acmePrefix = '/.well-known/acme-challenge/';
-
-if (str_starts_with($requestPath, $acmePrefix)) {
-    $token = substr($requestPath, strlen($acmePrefix));
-
-    if ($token !== '' && preg_match('/^[A-Za-z0-9._-]+$/', $token) === 1) {
-        $challengePath = __DIR__.'/.well-known/acme-challenge/'.$token;
-
-        if (is_file($challengePath)) {
-            header('Content-Type: text/plain; charset=UTF-8');
-            readfile($challengePath);
-            exit;
-        }
-    }
-
-    http_response_code(404);
-    echo 'Not Found';
-    exit;
-}
-
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
